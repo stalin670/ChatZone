98fa436d9fef72f55aedf08c6ada1f36f225aaec
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-19 17:01:25 +0530

    handle cors policy

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data.js");
+const cors = require("cors");
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8001;
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.get("/", (req, res) => {
   return res.status(200).json({ data: "Api is running successfully" });
 });
 
 app.get("/api/chat", (req, res) => {
-  return res.status(200).send(chats);
+  return res.status(200).json({ chats });
 });
 
 app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((chat) => chat._id === req.params.id);
-  return res.status(200).send(singleChat);
+  return res.status(200).json({ singleChat });
 });
 
 app.listen(PORT, () => {
