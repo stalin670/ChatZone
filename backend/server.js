9ccc43e7782abe0ac32d4b53f736b8b8b3278cc5
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-19 15:16:07 +0530

    created server and chat api for dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data.js");
 
 const app = express();
-const PORT = 8000;
+dotenv.config();
+const PORT = process.env.PORT || 8001;
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ data: "Api is running successfully" });
+});
+
+app.get("/api/chat", (req, res) => {
+  return res.status(200).send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  return res.status(200).send(singleChat);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
