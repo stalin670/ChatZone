a63f7f9a45f7bc2e8b1f58419c8841ba6d947ad4
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-19 17:01:13 +0530

    displaying dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage.js";
+import ChatPage from "./Pages/ChatPage.js";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
