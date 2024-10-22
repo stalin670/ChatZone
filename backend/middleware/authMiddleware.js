e40276dfc8531af0f42b2aa7d2d99446cfcbf392
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-22 17:46:03 +0530

    added middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel.js");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      //decodes token id
+      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err)
+          res.status(403).json({ error: true, message: "Invalid token" });
+
+        req.user = user;
+        next();
+      });
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+};
+
+module.exports = { protect };
