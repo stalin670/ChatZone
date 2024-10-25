ea5519cd2aed9c442eea7dd94e58d218f958b4d2
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-25 23:15:23 +0530

    fixing : updates

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -22,7 +22,7 @@ import UserBadgeItem from "../UserAvatar/UserBadgeItem";
 import axios from "axios";
 import UserListItem from "../UserAvatar/UserListItem";
 
-const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
+const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain, fetchMessages }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [groupChatName, setGroupChatName] = useState();
   const [search, setSearch] = useState("");
@@ -66,6 +66,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
         ? setSelectedChat()
         : setSelectedChat(data.removed);
       setFetchAgain(!fetchAgain);
+      fetchMessages();
       setLoading(false);
     } catch (error) {
       toast({
