7e410aabc3191b71600243e80430ffc7705ba883
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-25 19:19:50 +0530

    created sendMessage api end point

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -2,7 +2,48 @@ const Message = require("../models/messageModel");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModels");
 
-const sendMessage = async (req, res) => {};
+const sendMessage = async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.status(400).json({
+      success: false,
+      message: "Content or Id not available",
+    });
+  }
+
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var messages = await Message.create(newMessage);
+
+    messages = await messages.populate("sender", "name pic");
+    messages = await messages.populate("chat");
+    messages = await User.populate(messages, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: messages });
+
+    res.status(200).json({
+      success: true,
+      message: "Messages updated successfully",
+      messages,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
 
 const allMessages = async (req, res) => {};
 
