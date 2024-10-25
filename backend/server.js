f52c159451e2dfd687f4070da522d6decadbe349
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-26 01:34:37 +0530

    finishing realtime messaging setup

--- backend/server.js
+++ backend/server.js
@@ -57,4 +57,21 @@ io.on("connection", (socket) => {
     socket.join(room);
     console.log("User Joined Room : " + room);
   });
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("Chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
