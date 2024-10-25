5e23b6b6fb2441ee0f5898b4c4c0f70b8dc1970f
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-25 23:47:30 +0530

    fixing : updates

--- backend/server.js
+++ backend/server.js
@@ -47,4 +47,9 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
   console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
 });
