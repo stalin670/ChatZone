443a6219bfe4bb1563f6f2f78ddbdbf2cd2a42ad
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-25 23:40:21 +0530

    setup io connection at server

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,17 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
 });
